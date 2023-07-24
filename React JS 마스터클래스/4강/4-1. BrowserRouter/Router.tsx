f688a1993084d40bc4fe118bb0b4dc2c82c631ff
Win K <108077414+wink68@users.noreply.github.com>
2023-07-24 14:06:07 +0900

    Create Router.tsx

---
+++ React JS 마스터클래스/4강/4-1. BrowserRouter/Router.tsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import About from './screens/About';
+import Home from './screens/Home';
+
+function Router() {
+    return <BrowserRouter>
+        <Header />
+        <Routes>
+            <Route path='/' element={<Home />}></Route>
+            <Route path='/about' element={<About />} />
+        </Routes>
+    </BrowserRouter>
+}
+
+export default Router;
