7fbea79d2ee5921465b5e1e1f04bd5cd8f9a41b0
Win K <108077414+wink68@users.noreply.github.com>
2023-07-25 16:31:46 +0900

    Create Home.tsx

---
+++ React JS 마스터클래스/4강: React Router/4-5. useParams/screens/Home.tsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+import { users } from "../db";
+
+function Home() {
+  return (
+    <div>
+      <h2>Users</h2>
+      <ul>
+        {users.map((user) => (
+            <li key={user.id}>
+                <Link to={`/users/${user.id}`}>{user.name}</Link>
+            </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+export default Home;
