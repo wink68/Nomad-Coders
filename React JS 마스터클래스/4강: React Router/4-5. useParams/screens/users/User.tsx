95a597c2a8ad9b2878241a86fda77f30b2055035
Win K <108077414+wink68@users.noreply.github.com>
2023-07-25 16:32:11 +0900

    Create User.tsx

---
+++ React JS 마스터클래스/4강: React Router/4-5. useParams/screens/users/User.tsx
@@ -0,0 +1,9 @@
+import { useParams } from "react-router-dom";
+import { users } from "../../db";
+
+// useParams() : 파리미터 값을 URL을 통해 받아 사용할 수 있음
+function User() {
+    const { userId } = useParams();
+    return <h1>User with it {userId} is named: {users[Number(userId)-1].name}</h1>;
+}
+export default User;
